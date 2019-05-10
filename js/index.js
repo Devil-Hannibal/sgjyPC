b1ed38f049ea7127b46207ab3aa294f780f058c2
Devil-Hannibal <bfzy4444@163.com>
2019-05-10 14:22:20 +0800

    主页轮播图  完事

---
+++ js/index.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+// 第三个轮播图
+    var ltb3 = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        paginationClickable: true,
+        loop: true,
+        //设定初始化时slide的索引
+        initialSlide: 0,
+        //自动播放
+        autoplay: true,
+        //滑动速度
+        speed: 500,
+        //滑动方向
+        direction: 'horizontal',
+    });
+
+});
