f2eff631914b0b9db18abe427c3691e83393f263
Devil-Hannibal <bfzy4444@163.com>
2019-05-13 16:34:52 +0800

    培训课程 静态完事

---
+++ js/trainCourse.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    $("#paging").jqPaginator({
+        totalPages: 50,
+        visiblePages: 5,
+        currentPage: 2,
+        first: '<li class="first"><a href="javascript:void(0);">首页<\/a><\/li>',
+        prev: '<li class="prev"><a href="javascript:void(0);"><i class="arrow arrow2"><\/i>上一页<\/a><\/li>',
+        next: '<li class="next"><a href="javascript:void(0);">下一页<i class="arrow arrow3"><\/i><\/a><\/li>',
+        last: '<li class="last"><a href="javascript:void(0);">末页<\/a><\/li>',
+        page: '<li class="page"><a href="javascript:void(0);">{{page}}<\/a><\/li>',
+        onPageChange: function (n) {
+            $("#demo2-text").html("当前第" + n + "页");
+        }
+    });
+    $("#paging li").click(function(){
+        $(this).addClass('active').siblings().removeClass('active')
+    })
+})
